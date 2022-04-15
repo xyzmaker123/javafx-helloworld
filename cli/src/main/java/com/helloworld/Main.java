365a5216168e377dcd0ba07dfba47ee410c07a1d
xyzmaker123 <xyzmaker123@protonmail.com>
2022-04-15 14:11:58 +0200

    Initial work on logging configuration

--- cli/src/main/java/com/helloworld/Main.java
+++ cli/src/main/java/com/helloworld/Main.java
@@ -1,7 +1,17 @@
 package com.helloworld;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is hello world");
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.error("Hello world.");
+
+        // print internal state
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusPrinter.print(lc);
     }
 }
